22444aa58deae0044503d7aec7d58961a8d60714
Jesse Leite <jesseleite@gmail.com>
2020-12-01 12:40:32 -0500

    Add option to update Statamic, or whole project, with more transparency.

--- src/UpdateCommand.php
+++ src/UpdateCommand.php
@@ -6,6 +6,8 @@ use Statamic\Cli\Concerns;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 class UpdateCommand extends Command
 {
@@ -42,8 +44,35 @@ class UpdateCommand extends Command
 
         $output->writeln(PHP_EOL.'<comment>NOTE: If you have previously updated using the CP, you may need to update the version in your composer.json before running this update!</comment>'.PHP_EOL);
 
-        $this->runCommands(['composer update statamic/cms --with-dependencies'], $input, $output);
+        $command = $this->updateCommand($input, $output);
+
+        $this->runCommands([$command], $input, $output);
 
         return 0;
+    }
+
+    /**
+     * Determine the update command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return string
+     */
+    protected function updateCommand(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+
+        $options = [
+            'Update Statamic and its dependencies [composer update statamic/cms --with-dependencies]',
+            'Update all project dependencies [composer update]',
+        ];
+
+        $question = new ChoiceQuestion('How would you like to update Statamic?', $options, 0);
+
+        $selection = $helper->ask($input, new SymfonyStyle($input, $output), $question);
+
+        return strpos($selection, 'statamic/cms --with-dependencies')
+            ? 'composer update statamic/cms --with-dependencies'
+            : 'composer update';
     }
 }
